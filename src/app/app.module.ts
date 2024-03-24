97c35bf265e7118acab2d52166678a4d73ac73b2
Danilo Almeida Alves <71449479+b0d3-crypt@users.noreply.github.com>
2024-03-24 13:41:16 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRotas } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRotas),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { SpotifyConfiguration } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor() { }
+
+  obterUrlLogin() {
+    const authEndPointing = `${SpotifyConfiguration.authEndPoint}?`;
+    const clinetId = `client_id=${SpotifyConfiguration.clientId}&`;
+    const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
+    const scopes = `scopes=${SpotifyConfiguration.scopes.join('%20')}&`;
+    const responseType = `response_type=token&show_dialog=true`;
+    return authEndPointing + clinetId + redirectUrl + scopes + responseType;
+  }
+}
