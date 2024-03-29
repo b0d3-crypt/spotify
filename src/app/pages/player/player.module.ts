3e0a56056f559d55c71f38ca2542ac731eec81af
Danilo Almeida Alves <71449479+b0d3-crypt@users.noreply.github.com>
2024-03-29 17:05:41 -0300

    update

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BotaoMenuComponent } from 'src/app/components/botao-menu/botao-menu.component';
 import { SidnavComponent } from 'src/app/components/sidnav/sidnav.component';
 import { PlayerComponent } from './player.component';
 import { PlayerRotas } from './player.routes';
@@ -10,7 +11,8 @@ import { PlayerRotas } from './player.routes';
 @NgModule({
   declarations: [
     PlayerComponent,
-    SidnavComponent
+    SidnavComponent,
+    BotaoMenuComponent,
   ],
   imports: [
     CommonModule, 
