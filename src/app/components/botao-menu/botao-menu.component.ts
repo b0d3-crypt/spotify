16daeb58f760536349981e5781b7331e997e1cd3
Danilo Almeida Alves <71449479+b0d3-crypt@users.noreply.github.com>
2024-03-30 20:20:52 -0300

    binding do tipo de icone

--- src/app/components/botao-menu/botao-menu.component.ts
+++ src/app/components/botao-menu/botao-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-botao-menu',
@@ -8,6 +9,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class BotaoMenuComponent implements OnInit {
 
   @Input() message: string;
+  @Input() typeIcon: IconDefinition;
   @Input() classSelected: boolean;
   @Output() click = new EventEmitter<void>()
   
