58614cb8e5d51bf17dbd30f7016e73a19db63b13
Danilo Almeida Alves <71449479+b0d3-crypt@users.noreply.github.com>
2024-04-05 14:31:27 -0300

    update

--- src/app/components/rodape-usuario/rodape-usuario.component.ts
+++ src/app/components/rodape-usuario/rodape-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { IUsuario } from 'src/app/interfaces/IUsuario';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-rodape-usuario',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RodapeUsuarioComponent implements OnInit {
 
-  constructor() { }
+  usuario: IUsuario = null;
+  logout = faSignOutAlt;
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private _spotifyService: SpotifyService,
+  ) { }
 
+  ngOnInit(): void { }
 }
